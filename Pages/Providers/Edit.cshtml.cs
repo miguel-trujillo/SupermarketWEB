8a951744f8617d185fa97c06ceb82cf37d94e840
miguel trujillo <miguel trujillo@DESKTOP-5OOIK3O>
2023-05-11 11:41:07 -0500

    Edit Providers

---
+++ Pages/Providers/Edit.cshtml.cs
@@ -0,0 +1,69 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class EditModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public EditModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null || _context.Providers == null)
+            {
+                return NotFound();
+            }
+
+            var provider = await _context.Providers.FirstOrDefaultAsync(m => m.Id == id);
+            if (provider == null)
+            {
+                return NotFound();
+            }
+            Provider = provider;
+            return Page();
+        }
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _context.Attach(Provider).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProviderExists(Provider.Id))
+                {
+                    return Page();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ProviderExists(int id)
+        {
+            return (_context.Providers?.Any(e => e.Id == id)).GetValueOrDefault();
+        }
+    }
+}
