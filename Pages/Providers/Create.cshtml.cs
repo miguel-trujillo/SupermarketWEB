c7012ba49495cfb9431a7dc84a6bd7687a2e1bf6
miguel trujillo <miguel trujillo@DESKTOP-5OOIK3O>
2023-05-11 11:37:59 -0500

    Create Providers

---
+++ Pages/Providers/Create.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Providers
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public Provider Provider { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Providers == null || Provider == null)
+            {
+                return Page();
+            }
+
+            _context.Providers.Add(Provider);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
