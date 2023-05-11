a50f8cbb6f9b64c5bf63496d6a20ea0fc3919986
miguel trujillo <miguel trujillo@DESKTOP-5OOIK3O>
2023-05-11 15:53:40 -0500

    Create Products

---
+++ Pages/Productos/Create.cshtml.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Productos
+{
+    public class CreateModel : PageModel
+    {
+        private readonly SupermarketContext _context;
+
+        public CreateModel(SupermarketContext context)
+        {
+            _context = context;
+        }
+
+        public IActionResult OnGet()
+        {
+            return Page();
+        }
+        [BindProperty]
+        public Producto Producto { get; set; } = default!;
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid || _context.Productos == null || Producto == null)
+            {
+                return Page();
+            }
+
+            _context.Productos.Add(Producto);
+            await _context.SaveChangesAsync();
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
